2d773d866a0fad4f7568b87c6ac1738740dc8a7e
Robert Coleman <git@robert.net.nz>
2021-12-17 02:39:46 +1300

    version bump

--- Config/config.php
+++ Config/config.php
@@ -3,7 +3,7 @@
 return [
     'name'        => 'Advanced Templates',
     'description' => 'Plugin extends default email template capabilities with TWIG block so you can use advanced scripting techniques like conditions, loops etc',
-    'version'     => '2.0',
+    'version'     => '2.1',
     'author'      => 'Dmitry Berezovsky',
     'services' => [
         'events' => [
